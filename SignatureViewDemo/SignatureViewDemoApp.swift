270c2ce70a2789ec36245278e93a3b0c0ba581d8
Selim Arda Eroğlu <selimardaeroglu@gmail.com>
2021-02-12 20:16:45 +0300

    Initial Commit

---
+++ SignatureViewDemo/SignatureViewDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SignatureViewDemoApp.swift
+//  SignatureViewDemo
+//
+//  Created by Selim Arda Eroğlu on 12.02.2021.
+//
+
+import SwiftUI
+
+@main
+struct SignatureViewDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
